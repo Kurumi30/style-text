dd3a4ba3cd1db1c158e5414e0ce27077affcb070
Kurumi30 <fernandoshengxinzhu@gmail.com>
2024-03-19 00:37:49 +0000

    remove showInfo function

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express"
 import { styleText, transformText } from "../controllers/main"
 
 export async function text(req: Request, res: Response) {
-  console.info(showInfo(req))
-
   const { q } = req.query
 
   if (!q) return res.status(400).json({ error: "No query" })
@@ -18,8 +16,6 @@ export async function text(req: Request, res: Response) {
 }
 
 export async function transform(req: Request, res: Response) {
-  console.info(showInfo(req))
-
   const { q } = req.query
 
   if (!q) return res.status(400).json({ error: "No query" })
@@ -38,24 +34,8 @@ export async function pageNotFound(req: Request, res: Response) {
 }
 
 export async function showEndpoints(req: Request, res: Response) {
-  console.info(showInfo(req))
-
   return res.status(200).json({
     endpoints: ["/text", "/transform"],
     params: ["?q="],
   })
-}
-
-function showInfo(req: Request) {
-  return {
-    headers: req.headers,
-    host: req.hostname,
-    ip: req.ip,
-    method: req.method,
-    params: req.params,
-    path: req.path,
-    protocol: req.protocol,
-    q: req.query.q,
-    url: req.url,
-  }
 }
